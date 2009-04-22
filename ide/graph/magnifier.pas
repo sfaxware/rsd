bc9f27a2a21c2d0eff20c589d3a39fc0d76fee85
mazen <mazen@81b3eab5-a0c8-4206-baff-40505f750318>
2009-04-22 21:10:35 +0000

    * Added missing file in last commit.
    git-svn-id: svn+ssh://svn.kammoun.org/home/mazen/FPC/dtsl/trunk@86 81b3eab5-a0c8-4206-baff-40505f750318

---
+++ ide/graph/magnifier.pas
@@ -0,0 +1,80 @@
+unit Magnifier;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Controls;
+
+type
+  TMagnifier = class(TGraphicControl)
+  private
+    FBounds: TRect;
+    FMagnification: Integer;
+    function GetMagnification: Real;
+    procedure SetOriginalBounds(B: TRect);
+    procedure DoMagnify;
+  public
+    constructor Create(AOwner: TComponent); override;
+    procedure Magnify(m:Real); virtual;
+    property OriginalBounds: TRect read FBounds write SetOriginalBounds;
+    property Magnification: Real read GetMagnification;
+  end;
+
+implementation
+
+constructor TMagnifier.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  FMagnification := 256; // 1.0
+end;
+
+procedure TMagnifier.DoMagnify;
+var
+  R: TRect;
+begin
+  R := FBounds;
+  with R do begin
+    //WriteLn('Old bounds (', Name, ') = ((', Left, ', ', Top, '), (', Right, ', ', Bottom, '))');
+    //WriteLn('FMagnification = ', FMagnification, ' <=> ', FMagnification / 256);
+    Left :=  FMagnification * Left div 256;
+    Top :=  FMagnification * Top shr 8;
+    Right :=  FMagnification * Right shr 8;
+    Bottom :=  FMagnification * Bottom shr 8;
+    //WriteLn('New bounds (', Name, ') = ((', Left, ', ', Top, '), (', Right, ', ', Bottom, '))');
+  end;
+  BoundsRect := R;
+end;
+
+function TMagnifier.GetMagnification: Real;
+begin
+  Result := FMagnification / 256;
+end;
+
+procedure TMagnifier.SetOriginalBounds(B: TRect);
+begin
+  //with B do begin
+    //WriteLn('Changing bounds (', Name, ') = ((', Left, ', ', Top, '), (', Right, ', ', Bottom, '))');
+  //end;
+  FBounds := B;
+  Magnify(Magnification);
+end;
+
+procedure TMagnifier.Magnify(m:Real);
+var
+  i: Integer;
+  Component: TComponent;
+begin
+  FMagnification := Round(m * 256);
+  DoMagnify;
+  for i := 0 to ComponentCount - 1 do begin
+    Component := Components[i];
+    if Component is TMagnifier then with Component as TMagnifier do begin
+      Magnify(m);
+    end;
+  end;
+end;
+
+end.
+
