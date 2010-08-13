f292db5a9ad38611092cd5f8faa88c392aab34df
mazen <mazen@81b3eab5-a0c8-4206-baff-40505f750318>
2010-08-13 18:04:59 +0000

    Fixed dtslcore package dependency
    git-svn-id: svn+ssh://svn.kammoun.org/home/mazen/FPC/dtsl/trunk@207 81b3eab5-a0c8-4206-baff-40505f750318

---
+++ core/dtslcore.pas
@@ -0,0 +1,17 @@
+{ Ce fichier a Ã © t Ã © automatiquement cr Ã © Ã © par Lazarus. Ne pas l ' Ã © 
+  diter ! 
+  Cette source est seulement employ Ã © e pour compiler et installer le paquet.
+ }
+
+unit dtslcore; 
+
+interface
+
+uses
+    FifoBasics, BlockBasics, Blocks, Designs, Probes, Sources, Reals, Complex, 
+  ComplexMatrix, ComplexVectors, Filters, IntegerVectors, LMSfilter, 
+  LongIntVectors, RealMatrix, RealVectors, ShortIntVectors;
+
+implementation
+
+end.
