8c858d1346ae7c6d7aef24de5dd6d8c6d99a7709
mazen <mazen@ac704d9c-649c-4f73-85c4-9a6a4565dba8>
2012-03-28 20:40:51 +0000

    * Imporved painting of graph components.
    git-svn-id: https://svn.kammoun.org/svn/extprojects/dtsl/trunk@18 ac704d9c-649c-4f73-85c4-9a6a4565dba8

--- ide/graph/graphcomponents.pas
+++ ide/graph/graphcomponents.pas
@@ -47,57 +47,33 @@ begin
   inherited Create(AOwner);
   Width := 100;
   Height := 100;
-  Left := Random(256);
-  Top := Random(256);
-  Color := clRed;
-  Caption:= 'Hello';
 end;
 
 procedure TCGraphBlock.Paint;
 var
   PaintRect: TRect;
-  OffsetCap: TPoint;
-  ClientSize, TextSize: TSize;
   TXTStyle : TTextStyle;
 begin
-  WriteLn('TCGraphBlock.Paint ',Name,':',ClassName,' Parent.Name=',Parent.Name);
+  //WriteLn('TCGraphBlock.Paint ',Name,':',ClassName,' Parent.Name=',Parent.Name);
   PaintRect:=ClientRect;
-  ClientSize.cx:= PaintRect.Right - PaintRect.Left;
-  ClientSize.cy:= PaintRect.Bottom - PaintRect.Top;
-  TextSize.CY := PaintRect.Bottom - PaintRect.Top;
-  TextSize.CX := PaintRect.Right - PaintRect.Left;
-  OffsetCap.X:= (ClientSize.cx - TextSize.cx) div 2;
-  OffsetCap.Y:= (ClientSize.cy - TextSize.cy) div 2;
-
-  if Caption <> '' then
-  begin
-    TXTStyle := Canvas.TextStyle;
-    with TXTStyle do begin
-      Opaque := False;
-      Clipping := True;
-//      ShowPrefix := ShowAccelChar;
-      Alignment := taCenter;
-      Layout := tlCenter;
-    end;
-    // set color here, otherwise SystemFont is wrong if the button was disabled before
-    Canvas.Font.Color := Font.Color;
-
-    With PaintRect, OffsetCap do begin
-      Left := Left + X;
-      Top := Top + Y;
-    end;
-
-    with Canvas do begin
-      If not Enabled then begin
-        Font.Color := clBtnHighlight;
-        OffsetRect(PaintRect, 1, 1);
-        TextRect(PaintRect, PaintRect.Left, PaintRect.Top, Caption, TXTStyle);
-        Font.Color := clBtnShadow;
-        OffsetRect(PaintRect, -1, -1);
-      end;
-      WriteLn('TCGraphBlock.Paint PaintRect=',PaintRect.Left,', ',PaintRect.TOp,', ',PaintRect.Right,', ',PaintRect.Bottom,', ',caption,', ', TXTStyle.SystemFont);
+  with Canvas do begin
+    //WriteLn('TCGraphBlock.Paint PaintRect=',PaintRect.Left,', ',PaintRect.TOp,', ',PaintRect.Right,', ',PaintRect.Bottom,', ',caption,', ', TXTStyle.SystemFont);
+    If not Enabled then
+      Color := clBtnShadow
+    else
       Color:= Self.Color;
-      FillRect(PaintRect);
+    Rectangle(PaintRect);
+    if Caption <> '' then begin
+      TXTStyle := Canvas.TextStyle;
+      with TXTStyle do begin
+        Opaque := False;
+        Clipping := True;
+        ShowPrefix := False;
+        Alignment := taCenter;
+        Layout := tlCenter;
+      end;
+    // set color here, otherwise SystemFont is wrong if the button was disabled before
+      Font.Color := Self.Font.Color;
       TextRect(PaintRect, PaintRect.Left, PaintRect.Top, Caption, TXTStyle);
     end;
   end;
