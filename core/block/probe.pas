836782f3aafa6891313c9ae65ff64cdbe4590ee8
mazen <mazen@ac704d9c-649c-4f73-85c4-9a6a4565dba8>
2012-03-28 21:02:43 +0000

    * File dump probe is now working.
    git-svn-id: https://svn.kammoun.org/svn/extprojects/dtsl/trunk@138 ac704d9c-649c-4f73-85c4-9a6a4565dba8

--- core/block/probe.pas
+++ core/block/probe.pas
@@ -9,32 +9,57 @@ uses
 
 type
   TProbe = class(TBlock)
+  published
+    Input: TInputPort;
+  end;
+
+  TFileDumpProbe = class(TProbe)
   private
     FFile: Text;
     FFileName:string;
     procedure SetFileName(AFileName: string);
   public
     procedure Execute; override;
+    destructor Destroy; override;
+  published
     property FileName: string read FFileName write SetFileName;
   end;
 
 implementation
 
-procedure TProbe.SetFileName(AFileNAme: string);
+uses
+  BlockBasics;
+
+procedure TFileDumpProbe.SetFileName(AFileNAme: string);
 begin
+  //WriteLn(FuncB('TFileDumpProbe.SetFileName'), 'AFileNAme = ', AFileNAme);
   if FFileName = AFileNAme then
     Exit;
   if FFileName <> '' then begin
     Close(FFile);
   end;
-  Assign(FFile, FFileName);
+  FFileName := AFileName;
+  System.Assign(FFile, FFileName);
   ReWrite(FFile);
-  FFileName := AFileNAme;
+  //WriteLn(FuncE('TFileDumpProbe.SetFileName'), 'FFileName = ', FFileName);
 end;
 
-procedure TProbe.Execute;
+procedure TFileDumpProbe.Execute;
+var
+  Sample: Integer;
 begin
-  WriteLn(FFile, 'Input[0]');
+  //WriteLn(FuncB('TFileDumpProbe.Execute'));
+  with Input do while not IsEmpty do begin
+    Pop(Sample);
+    //WriteLn(FuncC('TFileDumpProbe.Execute'), 'IsEmpty = ', IsEmpty, ', Sample = ', Sample);
+    WriteLn(FFile, Sample);
+  end;
+  //WriteLn(FuncE('TFileDumpProbe.Execute'));
+end;
+
+destructor TFileDumpProbe.Destroy;
+begin
+  Close(FFile);
 end;
 
 end.
