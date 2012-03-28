1186e8684e51252145352e770cd8d5dd0cdcd4b5
mazen <mazen@ac704d9c-649c-4f73-85c4-9a6a4565dba8>
2012-03-28 21:00:32 +0000

    * Moved resource handling to Designs unit. * Added debug.
    git-svn-id: https://svn.kammoun.org/svn/extprojects/dtsl/trunk@125 ac704d9c-649c-4f73-85c4-9a6a4565dba8

--- core/block/designs.pas
+++ core/block/designs.pas
@@ -9,13 +9,39 @@ uses
 
 type
   TDesign = class(TBlock)
+  public
+    constructor Create(AOwner: TBlock); override;
+    constructor Create(AOwner: TDesign); virtual;
     procedure Run;
   end;
 
 implementation
 
 uses
-  Classes;
+  Classes, LResources;
+
+constructor TDesign.Create(AOwner: TBlock);
+begin
+  if AOwner is TDesign then begin
+    Create(AOwner as TDesign);
+  end;
+end;
+
+constructor TDesign.Create(AOwner: TDesign);
+var
+  cn: string;
+begin
+  if Assigned(AOwner) then begin
+    cn := AOwner.ClassName;
+  end else begin
+    cn := 'nil';
+  end;
+  WriteLn('>>TDesign.Create(AOwner: TDesign): Name = ', Name, ', AOwner.ClassName = ', cn);
+  inherited Create(AOwner);
+  if not InitResourceComponent(Self, TDesign) then
+    WriteLn('Failure');
+  WriteLn('<<TDesign.Create(AOwner: TDesign): Name = ', Name, ', AOwner.ClassName = ', cn);
+end;
 
 procedure TDesign.Run;
 begin
