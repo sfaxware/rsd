d9d75d7c95025aa59241293f017cf5b5c23a9551
mazen <mazen@ac704d9c-649c-4f73-85c4-9a6a4565dba8>
2012-03-28 20:49:04 +0000

    * Changed load/store to be handled by design and block objects instead of main window.
    git-svn-id: https://svn.kammoun.org/svn/extprojects/dtsl/trunk@63 ac704d9c-649c-4f73-85c4-9a6a4565dba8

--- ide/graph/designgraph.pas
+++ ide/graph/designgraph.pas
@@ -11,7 +11,7 @@ type
   TCGraphDesign = class(TScrollBox)
   private
     _Blocks:TFPList;
-    procedure StreamBlock(data, arg: pointer);
+    procedure StreamBlock(Block: TCGraphBlock;var Project: TXMLConfig);
   public
     SelectedBlock:TCGraphBlock;
     SelectedInputPort: TCGraphInputPort;
@@ -19,15 +19,18 @@ type
     constructor Create(AOwner: TComponent); override;
     function BlockCount: Integer; inline;
     function CreateNewBlock: TCGraphBlock; virtual;
+    function Load(Path: string; const Project: TXMLConfig): Boolean;
+    function Save(DesignName: string; var Project: TXMLConfig): Boolean;
     procedure ConnectPorts(Sender: TObject);
     procedure InsertBlock(Block:TCGraphBlock);
-    procedure RemoveBlock(Block:TCGraphBlock);
+    procedure RemoveBlock(var Block:TCGraphBlock);
     procedure SelectBlock(Sender: TObject);
-    procedure Stream(DesignName: string; var Project: TXMLConfig);
   end;
   TScrollBox = class(TCGraphDesign);
 
 implementation
+uses
+  Graphics, LFMTrees;
 
 constructor TCGraphDesign.Create(AOwner: TComponent);
 begin
@@ -83,9 +86,11 @@ begin
   _blocks.Add(Block);
 end;
 
-procedure TCGraphDesign.RemoveBlock(Block:TCGraphBlock);
+procedure TCGraphDesign.RemoveBlock(var Block:TCGraphBlock);
 begin
   _blocks.Remove(Block);
+  Block.Destroy;
+  Block := nil;
 end;
 
 procedure TCGraphDesign.SelectBlock(Sender: TObject);
@@ -98,24 +103,108 @@ begin
   end;
 end;
 
-procedure TCGraphDesign.StreamBlock(data, arg: pointer);
+procedure TCGraphDesign.StreamBlock(Block: TCGraphBlock;var Project: TXMLConfig);
 var
   Path: string;
 begin
-  with TXMLConfig(arg), TCGraphBlock(data) do begin
+  with Project, Block do begin
     Save;
     Path := 'design/blocks/' + Name + '/';
     SetValue(Path + 'name', Caption);
   end;
 end;
 
-procedure TCGraphDesign.Stream(DesignName: string; var Project: TXMLConfig);
+function TCGraphDesign.Load(Path: string; const Project: TXMLConfig): Boolean;
+  function GetPropertyValue(BlockDescription: TLFMTree; PropertyName: string): string;
+  var
+    PropertyNode: TLFMPropertyNode;
+    ValueNode: TLFMTreeNode;
+    c: char;
+    p: integer;
+  begin
+    Result := '';
+    PropertyNode := BlockDescription.FindProperty(PropertyName, nil);
+    ValueNode := PropertyNode.Next;
+    if ValueNode.TheType = lfmnValue then with ValueNode as TLFMValueNode do
+      case ValueType of
+        lfmvString: Result := ReadString;
+        lfmvInteger: begin
+          p := StartPos;
+          c := Tree.LFMBuffer.Source[p];
+          while c in ['0'..'9'] do begin
+            Result += c;
+            p += 1;
+            c := Tree.LFMBuffer.Source[p]
+          end;
+        end;
+      end;
+    WriteLn('GetPropertyValue(', PropertyName, ') = "', Result, '"');
+  end;
+var
+  BlockPath: string;
+  BlocksCount: integer;
+  BlockDescription: TLFMTree;
+begin
+  with Project do begin
+    for BlocksCount := 1 to GetValue(Path + 'count', 0) do begin
+      BlockPath := 'Block' + IntToStr(BlocksCount);
+      WriteLn('Loading "', BlockPath, '"');
+      if Assigned(SelectedBlock) then
+        SelectedBlock.Selected := False;
+      SelectedBlock := TCGraphBlock.Create(Self);
+      with SelectedBlock do begin
+        Parent := Self;
+        try
+          Name := 'Block' + IntToStr(BlocksCount);
+        except
+          WriteLn('Invalid block name "', BlockPath, '"');
+          Destroy;
+          SelectedBlock := nil;
+          continue;
+        end;
+        BlockDescription := GetDescription;
+        if not Assigned(BlockDescription) then begin
+          WriteLn('BlockDescription = nil');
+          SelectedBlock.Free;
+          continue;
+        end;
+        Left := StrToInt(GetPropertyValue(BlockDescription, BlockPath + '.Left'));
+        Top := StrToInt(GetPropertyValue(BlockDescription, BlockPath + '.Top'));
+        Color := clRed;
+        Caption := GetPropertyValue(BlockDescription, BlockPath + '.Name');
+        OnClick := @SelectBlock;
+        OnDblClick := Self.OnDblClick;
+        Selected := True;
+        PopupMenu := Self.PopupMenu;
+      end;
+      InsertBlock(SelectedBlock);
+    end;
+  end;
+end;
+
+function TCGraphDesign.Save(DesignName: string; var Project: TXMLConfig): Boolean;
+var
+  Component: TComponent;
+  i: Integer;
+  f: System.Text;
 begin
   with Project do begin
     SetValue('design/name', DesignName);
     SetValue('design/blocks/count', _Blocks.Count);
   end;
-  _Blocks.ForEachCall(@StreamBlock, Project);
+  System.Assign(f, '/tmp/Design.lfm');
+  ReWrite(f);
+  for i := 0 to ComponentCount - 1 do begin
+    Component := Components[i];
+    if Component is TCGraphConnector then with Component as TCGraphConnector do begin
+      Save(f);
+    end else if Component is TCGraphBlock then with Component as TCGraphBlock do begin
+      //Save();
+      StreamBlock(Component as TCGraphBlock, Project);
+    end; 
+  end;
+  Close(f);
+  Result := True;
 end;
 
 {procedure Register;
