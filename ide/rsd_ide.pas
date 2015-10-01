1a98ba2efde6898c77f6ec8c9b9954db43d96aa3
Mazen NEIFER <mazen@lt-mazen.nestwave.com>
2015-10-01 09:07:38 +0200

    Fix segmentation fault caused by a missing unit ExtToolEditDlg that is required to initialize a variable used by the package manager.

--- ide/rsd_ide.pas
+++ ide/rsd_ide.pas
@@ -7,7 +7,8 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, mainWindow, BlockProperties, PackagesManager, Configuration;
+  PackagesManager, ExtToolEditDlg,
+  Forms, mainWindow, BlockProperties, Configuration;
 
 var
   ProjectFileName: string;
